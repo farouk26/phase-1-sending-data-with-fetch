2a2249be432d3e072baf4ac69dc029bfd76437fe
Zemmouri Farouk <faroukwebdesign@gmail.com>
2023-09-06 20:18:59 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+const body = document.querySelector('body');
+function submitData(name, email) {
+    const data = {
+      name: name,
+      email: email,
+    };
+  
+    const dataObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+  
+    return fetch("http://localhost:3000/users", dataObject)
+    .then((res) => res.json())
+    .then(({ id }) => (body.innerHTML = id))
+    .catch(({ message }) => (body.innerHTML = message));
+  }
+//   submitData("Farouk", "test@test.com")
+//   .then(function (response){
+//     console.log("Response:", response);
+//     })
+//     .catch(function(error){
+//         console.log("Error:", error);
+//   })
+  
